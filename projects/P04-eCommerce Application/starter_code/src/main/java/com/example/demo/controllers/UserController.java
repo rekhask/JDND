ff01c7ede036c6071a8f427caae9345c79a14e76
Rekha <rekhask@hotmail.com>
2020-02-16 20:55:48 -0500

    Two new fields and BcryptPassword encoder
    Added two additional fields password and confirm password and BCryptPasswordEncoder to salt password. Also added password field validation

--- projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import com.example.demo.model.requests.CreateUserRequest;
 
 @RestController
 @RequestMapping("/api/user")
+
 public class UserController {
 	
 	@Autowired
@@ -27,6 +29,9 @@ public class UserController {
 	
 	@Autowired
 	private CartRepository cartRepository;
+
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@GetMapping("/id/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
@@ -43,11 +48,17 @@ public class UserController {
 	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
 		User user = new User();
 		user.setUsername(createUserRequest.getUsername());
+		user.setPassword(createUserRequest.getPassword());
 		Cart cart = new Cart();
 		cartRepository.save(cart);
 		user.setCart(cart);
+
+		if (createUserRequest.getPassword().length()<7 || !createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())) {
+		    //log.info ("Error with user password. Cannot create user {}", createUserRequest.getUsername());
+		    return ResponseEntity.badRequest().build();
+        }
+		user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
 		userRepository.save(user);
 		return ResponseEntity.ok(user);
 	}
-	
 }
